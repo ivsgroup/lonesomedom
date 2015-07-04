e61dbc4c7d9cba6e87ca71cfc11e7e244dfcfd96
François Leurent <131.php@cloudyks.org>
2015-07-04 23:54:06 +0200

    Drop mootools dependencies

--- lonesomedom.js
+++ lonesomedom.js
@@ -1,6 +1,18 @@
 var Class = require('uclass');
 var ucss  = require('microcss');
 
+
+var $n = function(type, attrs){
+  var out = document.createElement(type);
+  for(var k in attrs)
+      out[k] = attrs[k];
+  out.inject = function(parent, top){
+    parent.insertBefore(out, top ? parent.firstChild : null);
+    return out;
+  }
+  return out;
+}
+
 module.exports = function(anchor){
   return (new LonesomeDom(anchor)).process();
 }
@@ -17,20 +29,21 @@ var LonesomeDom = new Class({
     var allcss = ucss(this.anchor);
 
     while(container != document){ 
-      var foo = new Element(container.nodeName, {'class': container.className, 'style' : container.style, id: container.id});
+      var foo = $n(container.nodeName, {'class': container.className, 'style' : container.style});
+      if(container.id) foo.id = container.id;
       if(container == this.anchor)
         foo.innerHTML = this.anchor.innerHTML;
       else {
-        foo.adopt(lastfoo);
+        lastfoo.inject(foo);
       }
       container = container.parentNode;
 
       lastfoo = foo;
     }
 
-    var head = (new Element('head')).inject(lastfoo, 'top');
-    (new Element('meta', {'http-equiv': "Content-Type", content: 'text/html', charset: 'utf-8'})).inject(head);
-    (new Element('style', {type: "text/css", text: allcss })).inject(head);
+    var head = $n('head').inject(lastfoo, 'top');
+    $n('meta', {'http-equiv': "Content-Type", content: 'text/html', charset: 'utf-8'}).inject(head);
+    $n('style', {type: "text/css", innerText: allcss }).inject(head);
 
     return lastfoo;
   }
